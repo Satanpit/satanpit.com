e62d706506b7ba641d5918581a162f7dcaaf5ee2
Satanpit <alex.hyrenko@gmail.com>
2017-05-08 10:11:15 +0300

    Change structure

--- gulpfile.js
+++ gulpfile.js
@@ -8,31 +8,14 @@ const iconFont = require('gulp-iconfont');
 const iconFontCSS = require('gulp-iconfont-css');
 
 gulp.task('css', () => (
-  gulp.src('./src/css/main.css').pipe(postcss([
+  gulp.src('./css/main.css').pipe(postcss([
     cssImports, cssVariables, nestedcss
-  ])).pipe(dest('./dist/css', {ext: 'min.css'})).pipe(gulp.dest(''))
+  ])).pipe(dest('./css', {ext: 'min.css'})).pipe(gulp.dest(''))
 ));
 
-gulp.task('copy:img', () => (
-  gulp.src('./src/img/*.*').pipe(gulp.dest('./dist/img'))
-));
-
-gulp.task('copy:fonts', () => (
-  gulp.src('./src/fonts/**').pipe(gulp.dest('./dist/fonts'))
-));
-
-gulp.task('copy:html', () => (
-  gulp.src('./src/index.html').pipe(gulp.dest('./dist'))
-));
-
-gulp.task('copy:favicon', () => (
-  gulp.src('./src/favicon.png').pipe(gulp.dest('./dist'))
-));
-
-gulp.task('copy', ['copy:img', 'copy:fonts', 'copy:html', 'copy:favicon']);
 
 gulp.task('icons', () => (
-  gulp.src('./src/img/icons/*.svg').pipe(iconFontCSS({
+  gulp.src('./img/icons/*.svg').pipe(iconFontCSS({
     fontName: 'icons-font',
     path: './css-icons-template.css',
     targetPath: '../../css/icons.css',
@@ -41,11 +24,11 @@ gulp.task('icons', () => (
     fontName: 'icons-font',
     prependUnicode: false,
     formats: ['ttf', 'eot', 'woff', 'woff2', 'svg']
-  })).pipe(gulp.dest('./dist/fonts/icons-font'))
+  })).pipe(gulp.dest('./fonts/icons-font'))
 ));
 
-gulp.task('build', ['icons', 'css', 'copy']);
+gulp.task('build', ['icons', 'css']);
 
 gulp.task('dev', ['build'], () => {
-  gulp.watch(['./src/css/**'], ['css']);
+  gulp.watch(['./css/**'], ['css']);
 });
